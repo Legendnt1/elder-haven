f4069de6a667aba5c9f47128f7f83f22ee114795
Cesar- <legende.e@hotmail.com>
2024-10-22 00:28:38 -0500

    Colocando el docs las mejoras del contact

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LET7VISO.mjs";import"./chunk-RI2JTARK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-M47AAOHW.mjs";import"./chunk-RI2JTARK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
