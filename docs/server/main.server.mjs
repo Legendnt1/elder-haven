0eb7cd7765a3f6e4cbf93ff34e99e386fc643f1c
Cesar- <legende.e@hotmail.com>
2024-10-25 15:17:51 -0500

    Cambiando las fotos .png por archivos .webp

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NEVDJRAT.mjs";import"./chunk-RI2JTARK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-4NRLTQWA.mjs";import"./chunk-RI2JTARK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
